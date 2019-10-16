d1a674f18df800e08d4a752064b3f84864f8e2ef
ChenRunyang <1730088644@qq.com>
2019-10-16 14:43:04 +0800

    1730088644@qq.com

--- 10to13.cpp
+++ 10to13.cpp
@@ -7,4 +7,5 @@ int main()
     char src[2][12][4]={{"tam","hel","maa","huh","tou","kes","hei","elo","syy","lok","mer","jou"},
     {"jan","feb","mar","apr","may","jun","jly","aug","sep","oct","nov","dec"}};
     cout<<src[1][2];
+    cout<<"ou";
 }
