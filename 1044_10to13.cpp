fc60de0cbf03e89475c9fa6c2254a5abef143b0f
ChenRunyang <1730088644@qq.com>
2019-10-18 00:25:35 +0800

    1730088644@qq.com

--- 1044_10to13.cpp
+++ 1044_10to13.cpp
@@ -1,11 +1,31 @@
 #include<iostream>
+#include<string>
+#include<ctype.h>
 
 using namespace std;
 
 int main()
 {
-    char src[2][12][4]={{"tam","hel","maa","huh","tou","kes","hei","elo","syy","lok","mer","jou"},
+    int num;
+    cin>>num;
+    cin.clear();
+    cin.ignore();
+    string src;
+    string det[num];
+    int tmp;
+    char mem[2][12][4]={{"tam","hel","maa","huh","tou","kes","hei","elo","syy","lok","mer","jou"},
     {"jan","feb","mar","apr","may","jun","jly","aug","sep","oct","nov","dec"}};
-    cout<<src[1][2];
-    cout<<"ou";
+    for(int i=0;i<num;i++)
+    {
+        getline(cin,src);
+        if(isdigit(src[0]))
+        {
+            tmp=stoi(src);
+            if(tmp>12)
+            {
+                cout<<mem[0][tmp/13];
+                tmp %=13;
+            }
+        }
+    }
 }
